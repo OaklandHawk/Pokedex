8d11c1357bf2564c89d6fbf4ce95e9bf2a27bc8f
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-06 09:40:35 -0700

    Added SearchBarDelegagte, updateViews function, and filled out searchBarButtonClicked func

--- Pokedex/View Controllers/PokemonDetailViewController.swift
+++ Pokedex/View Controllers/PokemonDetailViewController.swift
@@ -27,10 +27,45 @@ class PokemonDetailViewController: UIViewController {
     }
 	
 	func searchBarButtonClicked(_ searchBar: UISearchBar) {
+		guard let text = searchBar.text?.lowercased() else { return }
+		guard let pokeController = pokemonController else { return }
+		
+		pokeController.searchForPokemon(for: text) { (result) in
+			do {
+				let thisPokemon = try result.get()
+				DispatchQueue.main.async {
+					self.updateView(with: thisPokemon)
+				}
+				pokeController.getImage(at: thisPokemon.sprites.frontDefault, completion: { (result) in
+					do {
+						let image = try result.get()
+						DispatchQueue.main.async {
+							self.pokemonImageView.image = image
+						}
+					} catch {
+						NSLog("Could not load")
+					}
+				})
+				self.pokemon = thisPokemon
+			} catch {
+				NSLog("Error")
+			}
+		}
+	}
+
+	func updateView(with searchedPokemon: Pokemon) {
+		nameLabel.text = searchedPokemon.name
+		pokemonIdLabel.text = String(searchedPokemon.id)
+		pokemonTypeLabel.text = searchedPokemon.types[0]
+		pokemonAbilitiesLabel.text = searchedPokemon.abilities[0]
 		
 	}
     
 	@IBAction func savePokemonButton(_ sender: Any) {
 	}
+	
+}
+
+extension PokemonDetailViewController: UISearchBarDelegate {
 	
 }
