6b1972feba285a3e62a46bf68730cc495d868afa
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-06 08:39:10 -0700

    Set up storyboard and Pokemon Model

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,16 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Taylor Lyles on 9/6/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+	var name: String
+	var id: Int
+	var abilities: String
+	var types: String
+}
